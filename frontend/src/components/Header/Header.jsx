7d81c7a2ca4d192f20360e6fca106a097c201232
sahilh <sahilh3600@gmail.com>
2024-12-26 11:14:26 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import logo from "../../assets/logo.png";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="bg-white flex justify-between items-center fixed top-0 left-0 w-full z-20 p-4">
+      <div className="flex-shrink-0">
+        <a href="/">
+          <img src={logo} alt="Logo" className="h-10 w-15 object-contain" />
+        </a>
+      </div>
+
+      <div className="md:hidden text-gray text-4xl mr-2 cursor-pointer" onClick={toggleMenu}>
+        {isMenuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <ul
+        className={`flex-col md:flex-row md:flex items-center gap-4 text-gray font-semibold absolute md:static top-full left-0 w-full bg-[#ffffff] md:bg-transparent md:w-auto transition-transform transform md:translate-x-0 ${
+          isMenuOpen ? "translate-x-0 z-30" : "-translate-x-full"
+        } md:translate-x-0 md:flex-row z-20`}
+      >
+        <li className="hover:scale-105 transition-all duration-150">
+          <a href="#">Home</a>
+        </li>
+        <li className="hover:scale-105 transition-all duration-150">
+        <a href="#">About Us</a>
+        </li>
+        <li className="hover:scale-105 transition-all duration-150">
+        <a href="/">Service</a>
+        </li>
+        <li className="hover:scale-105 transition-all duration-150">
+          <a href="#">Contact Us</a>
+        </li>
+        <li className="flex items-center">
+  <a href="#">
+    <button className="bg-[#ff5733] text-white px-4 py-2 rounded-lg hover:bg-[#e67e22] transition-all duration-300 ease-in-out transform hover:scale-105">
+      Get Started
+    </button>
+  </a>
+</li>
+
+
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
